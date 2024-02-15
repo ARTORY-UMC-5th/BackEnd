e945b21201fabdf18b80c1b3d7b9909a7ba24f25
임원재 <80938176+yyytir777@users.noreply.github.com>
2024-02-16 00:23:54 +0900

    Feat : 프론트 수정사항 (scarpMember 수정)

--- src/main/java/com/example/demo/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/demo/domain/member/repository/MemberRepository.java
@@ -18,8 +18,9 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query("SELECT distinct m " +
             "FROM Member m " +
             "JOIN ScrapMember sm ON sm.fromMember.memberId = :memberId " +
-            "JOIN ScrapMember sm1 ON sm.toMember.memberId = sm1.toMember.memberId " +
-            "WHERE m.memberId = sm1.fromMember.memberId and m.memberId != :memberId and sm.isScrapped = false " )
+            "JOIN ScrapMember sm1 ON sm.toMember.memberId = sm1.fromMember.memberId " +
+            "JOIN ScrapMember sm2 ON m = sm2.fromMember and sm2.toMember = sm1.toMember " +
+            "WHERE m.memberId = sm1.toMember.memberId and m.memberId != :memberId and sm2.isScrapped = false " )
     Page<Member> recommendMember(Pageable pageable, Long memberId);
 
     @Query("select distinct m " +
