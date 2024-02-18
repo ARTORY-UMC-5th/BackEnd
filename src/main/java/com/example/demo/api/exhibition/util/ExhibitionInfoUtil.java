93c1a2f9298d1076c932ee647ca4d2c96ee214e1
임원재 <80938176+yyytir777@users.noreply.github.com>
2024-02-19 01:18:46 +0900

    Feat : 전시회 api 수정

--- src/main/java/com/example/demo/api/exhibition/util/ExhibitionInfoUtil.java
+++ src/main/java/com/example/demo/api/exhibition/util/ExhibitionInfoUtil.java
@@ -52,7 +52,7 @@ public class ExhibitionInfoUtil {
 
             //같은 전시회가 존재하면 pass
             Boolean isExist = exhibitionRepository.existsByExhibitionTitle(exhibitionInfo.getExhibition_title());
-            if (!isExist) continue;
+            if (isExist) continue;
 
             LocalDateTime currentDateTime = LocalDateTime.now();
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
@@ -80,7 +80,6 @@ public class ExhibitionInfoUtil {
             LocalDateTime exhibitionStartDate = LocalDateTime.parse(exhibition_start_date + " 00:00:00", timeFormatter);
             boolean is_started = currentDateTime.isAfter(exhibitionStartDate);
 
-            System.out.println("exhibitionInfo = " + exhibitionInfo);
             Exhibition exhibition = exhibitionConverter.convertToEntity(exhibitionInfo, exhibition_like_count, is_started, is_ended, "0분");
             exhibitionList.add(exhibition);
         }
