09ef6043f74377a8572499255dd4007031af6441
임원재 <80938176+yyytir777@users.noreply.github.com>
2024-02-18 21:25:05 +0900

    Feat :전시회 api (apiKey 숨김)

--- src/main/java/com/example/demo/api/exhibition/util/ExhibitionInfoUtil.java
+++ src/main/java/com/example/demo/api/exhibition/util/ExhibitionInfoUtil.java
@@ -30,7 +30,7 @@ public class ExhibitionInfoUtil {
 
     public void getInfo() {
 
-        String url = "http://openapi.seoul.go.kr:8088/42456b54756b6f7236386167426b68/json/culturalEventInfo/1/300/";
+        String url = "http://openapi.seoul.go.kr:8088/" + apiKey + "/json/culturalEventInfo/1/300/";
 
         RestTemplate restTemplate = new RestTemplate();
 
