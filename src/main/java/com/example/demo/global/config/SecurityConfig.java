a5ce36bd024263a6932f7e5becda29b525d92e66
Jang seungwoo <chanyou7@gmail.com>
2024-02-19 01:02:42 +0900

    토큰 시간 조정

--- src/main/java/com/example/demo/global/config/SecurityConfig.java
+++ src/main/java/com/example/demo/global/config/SecurityConfig.java
@@ -4,8 +4,7 @@ import com.example.demo.global.jwt.service.TokenManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+
 
 
 @Configuration
