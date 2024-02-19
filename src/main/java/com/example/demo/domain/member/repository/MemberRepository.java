f0857c771c9d27b74d83679205b2abee4997075e
임원재 <80938176+yyytir777@users.noreply.github.com>
2024-02-20 00:07:12 +0900

    Feat : 추천 멤버 수정

--- src/main/java/com/example/demo/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/demo/domain/member/repository/MemberRepository.java
@@ -21,13 +21,14 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
             "FROM Member m " +
             "JOIN ScrapMember sm ON sm.fromMember.memberId = :memberId " +
             "JOIN ScrapMember sm1 ON sm.toMember.memberId = sm1.fromMember.memberId " +
-            "JOIN ScrapMember sm2 ON sm2.fromMember = :memberId and sm2.toMember = sm1.toMember " +
+            "JOIN ScrapMember sm2 ON sm2.fromMember.memberId = :memberId and sm2.toMember = sm1.toMember " +
             "WHERE m = sm2.toMember and m.memberId != :memberId and sm2.isScrapped = false " )
     Page<Member> recommendMember(Pageable pageable, Long memberId);
 
     @Query("select distinct m " +
             "from Member m " +
-            "where m.memberId != :memberId " +
+            "join ScrapMember sm on sm.fromMember.memberId = :memberId " +
+            "where m.memberId != :memberId and (sm is null or sm.isScrapped != true) " +
             "order by rand() ")
     Page<Member> initRecommendMember(Pageable pageable, Long memberId);
 }
